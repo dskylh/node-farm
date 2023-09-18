3afd8854fbd36ff3449a7f8c5c030ecedeef4dd8
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-09-18 16:28:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+// this one executes after the create server because it is a top level function
+const replaceTemplate = (temp, product) => {
+  // let output = temp.replace(product.)
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.quantity);
+  output = output.replace(/{%ID%}/g, product.quantity);
+  if (!product.organic) output = output.replace(/{%ORGANIC%}/g, "not-organic");
+  return output;
+};
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+const overviewTemplate = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const cardTemplate = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const productTemplate = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  // OVERVIEW
+  if (pathName === "/overivew" || pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHTML = dataObject
+      .map((element) => replaceTemplate(cardTemplate, element))
+      .join("");
+    const output = overviewTemplate.replace("{%PRODUCTCARDS%}", cardsHTML);
+    res.end(output);
+  }
+  // PRODUCT PAGE
+  else if (pathName === "/product") {
+    res.end("this is the product");
+  }
+  // API
+  else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+  // PAGE NOT FOUND
+  else {
+    res.writeHead(404, {});
+    res.end("page not found 404");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to the port http://localhost:8000");
+});
---
+++ read-write.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+// Syncrhonous doo doo way of doing things
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `what in the hell nuga: ${textIn}`;
+// console.log(textOut);
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// Asyncrhonous chad way of doing things
+const asyncTextIn = fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  const asyncTextIn = fs.readFile(
+    `./txt/${data1}.txt`,
+    "utf-8",
+    (err, data2) => {
+      console.log(data2);
+
+      const asyncTextIn = fs.readFile(
+        "./txt/appendix.txt",
+        "utf-8",
+        (err, data3) => {
+          console.log(data3);
+
+          fs.writeFile(
+            "./txt/final.txt",
+            `${data2}\n${data3}`,
+            "utf-8",
+            (err) => {
+              console.log("stuff has been written");
+            }
+          );
+        }
+      );
+    }
+  );
+});
+console.log("wha");
