6fdf86f8d47bc9d54daf601d4271558c61f54ac4
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-09-21 17:14:21 +0300

    update gitignore and initialize npm

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 // this one executes after the create server because it is a top level function
-const replaceTemplate = (temp, product) => {
-  // let output = temp.replace(product.)
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) output = output.replace(/{%ORGANIC%}/g, "not-organic");
-  return output;
-};
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
 const overviewTemplate = fs.readFileSync(
